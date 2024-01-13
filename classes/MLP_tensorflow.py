251d73701ff248b6f08ab861c02fe044be8c5fd3
icarus64 <ellishogan95@gmail.com>
2024-01-12 20:30:14 -0500

    update

--- classes/MLP_tensorflow.py
+++ classes/MLP_tensorflow.py
@@ -4,29 +4,16 @@ from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Flatten, Dense
 import os
 
+from abstract_base_classes.ann_shell import ANN_Shell
 
-class Digit_Recognition_MLP:
-	def __init__(self, name='MLP_TF.keras'):
-		current_file = os.path.abspath(__file__)
-		cd = os.path.dirname(current_file)
-		models_dir = f'{cd}../models'
-		os.makedirs(models_dir, exist_ok=True)
-		location = os.path.join(models_dir, name) # Combo of [cwd]/dir/name
-		
-		# Define class properties
-		self.name = name
-		self.location = location
-		
-		# load current modal or make new if none exsists. 
+class Digit_Recognition_MLP(ANN_Shell):
+	def __init__(self):
 		self.model = load_model(l) if os.path.isfile(l) else self.build() 
 
 	def build(self, imageSize=[28, 28], layers=2):
 		model = Sequential([
-			# Flatten 28x28 images to a 1D array
 			Flatten(input_shape=(imageSize[0], imageSize[1])),
-			# Fully connected layer with 128 units and ReLU activation
 			*[Dense(128, activation='relu') for _ in range(layers)],
-			# Output layer with 10 units (one for each digit) and softmax activation
 			Dense(10, activation='softmax')
 		])
 		return model
@@ -48,9 +35,6 @@ class Digit_Recognition_MLP:
 										validation_data=(x_test, y_test))
 		print("Training Complete!")
 
-	def save(self):
-		self.model.save(self.location)
-		print("Model saved successfully.")
 		
 if __name__ == "__main__":
 	mlp = Digit_Recognition_MLP()
